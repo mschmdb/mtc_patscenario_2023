0b2746a3054fbdf3176efc610e6a215e709e6829
mschmdb <113426956+mschmdb@users.noreply.github.com>
2022-12-16 10:55:59 +0100

    fix?

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+        sveltekit(),
+    ],
+    optimizeDeps: {
+        include: ['@carbon/charts-svelte', '@carbon/charts']
+    },
 };
 
 export default config;
